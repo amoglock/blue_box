941b22cd557849b4f30dd409f553bac13f832695
amoglock <amoglock@gmail.com>
2023-09-21 16:19:42 +0400

    added router and lifespan

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.database.database_helper import db_helper
+from src.database.models import Base
+
+from src.core.views.router import router as raw_router
+
+
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    async with db_helper.engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+        yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router=raw_router)
