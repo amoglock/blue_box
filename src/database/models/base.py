f0eec3ed91be26eaccd684879f9e8eae40e1b68c
amoglock <amoglock@gmail.com>
2023-09-26 14:41:51 +0400

    change id line and add generate_uuid function

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -3,6 +3,10 @@ import uuid
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
 class Base(DeclarativeBase):
     __abstract__ = True
 
@@ -11,4 +15,4 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
 
-    id: Mapped[str] = mapped_column(primary_key=True, default=str(uuid.uuid4))
+    id: Mapped[str] = mapped_column(primary_key=True, default=generate_uuid)
