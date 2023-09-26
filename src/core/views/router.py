06b53ee33b12b3a80570a27e87edaa9ec5f6d555
amoglock <amoglock@gmail.com>
2023-09-26 14:44:01 +0400

    add get_raw_list function and comments

--- src/core/views/router.py
+++ src/core/views/router.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter, status, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.schemas import CreateRaw
 from src.core.schemas.raw_schemas import Raw
 from src.core.services.raw_service import RawService
-from src.database import db_helper
 
 
 router = APIRouter(tags=["Raw"])
@@ -15,4 +13,27 @@ async def post_raw_into_base(
         raw_in: CreateRaw,
         raw_service: RawService = Depends(),
 ):
+
+    """
+    Post new entry to the database
+
+    :param raw_in: incoming Raw instance as CreateRaw model
+    :param raw_service: RawService instance
+    :return: Raw model instance
+    """
+
     return await raw_service.post_raw(raw_in=raw_in)
+
+
+@router.get("/all_raw", response_model=list[Raw], status_code=status.HTTP_200_OK)
+async def get_raw_list(
+        raw_service: RawService = Depends(),
+):
+
+    """
+
+    :param raw_service:
+    :return:
+    """
+
+    return await raw_service.get_raw_list()
