b00b0c79d144a9c519652da756aca58678bcdc5d
amoglock <amoglock@gmail.com>
2024-02-06 16:19:25 +0400

    add some methods

--- src/raw_storage/service/service.py
+++ src/raw_storage/service/service.py
@@ -1,10 +1,15 @@
+from fastapi import Depends
+
 from src.raw_storage.models.models import Raw
+from src.raw_storage.repository.repository import RawRepository
 
 
 class RawStorageService:
-    def __init__(self):
-        self.storage = {}
+    def __init__(self, raw_repository: RawRepository = Depends()):
+        self.raw_repository = raw_repository
 
     async def arrival_raw(self, raw: Raw):
-        self.storage[raw.title] = raw
-        return self.storage
+        return await self.raw_repository.add_raw(raw)
+
+    async def get_storage(self):
+        return await self.raw_repository.get_storage()
