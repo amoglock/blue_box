be67c1c52c4a95f933f5356dceca20d9accf874a
amoglock <amoglock@gmail.com>
2024-04-04 20:35:46 +0400

    test_get_storage is work!!!))))

---
+++ tests/raw_storage/test_raw_storage.py
@@ -0,0 +1,33 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+
+from src.main import app
+from src.raw_storage.models.models import IncomingRaw
+from src.raw_storage.repository.repository import RawRepository
+
+test_raw = {
+    "title": "Dragon Fruit",
+    "group": "string",
+    "supplier": "Microsoft",
+    "incoming_amount": 0,
+    "delivery_date": "2024-02-19",
+    "production_date": "2024-02-19",
+    "expiration_date": "2024-02-19"
+}
+
+
+@pytest.mark.anyio
+async def test_get_storage():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://127.0.0.1:8000/raw") as ac:
+        response = await ac.get("/get_storage")
+        assert response.status_code == 200
+        assert response.json() == {
+            "data": "success",
+            "result": {}
+        }
+
+        response = await ac.get("/get_storage?name=unknown_name")
+        assert response.json() == {
+            "data": "Item unknown_name not found",
+            "result": None
+        }
