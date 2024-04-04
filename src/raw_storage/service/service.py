59381c71b7fecda31cb69efa6acce26fa85abb08
amoglock <amoglock@gmail.com>
2024-04-05 02:05:11 +0400

    Delete Depends() and change get_storage()

--- src/raw_storage/service/service.py
+++ src/raw_storage/service/service.py
@@ -1,18 +1,26 @@
+from typing import Annotated
+
 from fastapi import Depends
 
 from src.raw_storage.models.models import IncomingRaw
+from src.raw_storage.models.response_models import ResponseModel
 from src.raw_storage.repository.repository import RawRepository
 
 
 class RawStorageService:
-    def __init__(self, raw_repository: RawRepository = Depends()):
-        self.raw_repository = raw_repository
+    def __init__(self):
+        self.raw_repository = RawRepository()
+        self.response = ResponseModel()
 
     # async def arrival_raw(self, raw: IncomingRaw):
     #     return await self.raw_repository.add_raw(raw)
 
     async def get_storage(self, name):
-        result = await self.raw_repository.get_storage(name)
-        if name and not result:
-            return {"data": f"Item {name} not found", "result": result}
-        return {"data": "success", "result": result}
+        self.response.data = await self.raw_repository.get_storage(name)
+
+        if name and not self.response.data:
+            self.response.status = False
+            self.response.message = f"Item {name} not found"
+            return self.response
+
+        return self.response
