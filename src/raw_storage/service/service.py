60735c4afd8abc450ef8e7ea39cdf83b282dd4e3
amoglock <amoglock@gmail.com>
2024-04-04 20:35:16 +0400

    change RawService

--- src/raw_storage/service/service.py
+++ src/raw_storage/service/service.py
@@ -8,8 +8,11 @@ class RawStorageService:
     def __init__(self, raw_repository: RawRepository = Depends()):
         self.raw_repository = raw_repository
 
-    async def arrival_raw(self, raw: IncomingRaw):
-        return await self.raw_repository.add_raw(raw)
+    # async def arrival_raw(self, raw: IncomingRaw):
+    #     return await self.raw_repository.add_raw(raw)
 
-    async def get_storage(self, name=None):
-        return await self.raw_repository.get_storage(name)
+    async def get_storage(self, name):
+        result = await self.raw_repository.get_storage(name)
+        if name and not result:
+            return {"data": f"Item {name} not found", "result": result}
+        return {"data": "success", "result": result}
