c98559f1933b6cda8d714b395283c32bfc58ad4b
amoglock <amoglock@gmail.com>
2024-04-05 19:10:01 +0400

    some changes

--- src/raw_storage/repository/repository.py
+++ src/raw_storage/repository/repository.py
@@ -4,9 +4,9 @@ from src.raw_storage.models.models import IncomingRaw
 class RawRepository:
     storage = {}
 
-    # async def add_raw(self, raw: IncomingRaw) -> IncomingRaw:
-    #     self.storage[raw.title] = raw
-    #     return raw
+    async def add_raw(self, raw: IncomingRaw) -> IncomingRaw:
+        self.storage[raw.title] = raw
+        return raw
 
     async def get_storage(self, name):
         """
