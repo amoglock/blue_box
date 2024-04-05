45545bf40fd8c97a0cda383f1ad25a01cb825fe3
amoglock <amoglock@gmail.com>
2024-04-05 19:08:57 +0400

    add database files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from src.raw_storage.routers.raw_router import raw_router
+import src.models
+from src.database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+src.models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 app.include_router(raw_router)
