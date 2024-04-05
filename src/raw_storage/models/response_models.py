98f7f853a6c70b18dddf18f32e0838f51e458092
amoglock <amoglock@gmail.com>
2024-04-05 19:10:11 +0400

    some changes

--- src/raw_storage/models/response_models.py
+++ src/raw_storage/models/response_models.py
@@ -2,10 +2,10 @@ from typing import Union
 
 from pydantic import BaseModel, Field
 
-from src.raw_storage.models.models import IncomingRaw
+from src.raw_storage.models.models import IncomingRaw, RawResponse
 
 
 class ResponseModel(BaseModel):
     status: bool = Field(default=True)
     message: str | None = "success"
-    data: Union[IncomingRaw, dict] = {}
+    data: RawResponse | dict = {}
