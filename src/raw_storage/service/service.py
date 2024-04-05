45f6a03a6ec28fad9ed2ae7918ec783ee91b963f
amoglock <amoglock@gmail.com>
2024-04-05 19:10:25 +0400

    some changes

--- src/raw_storage/service/service.py
+++ src/raw_storage/service/service.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from src.raw_storage.models.models import IncomingRaw
+from src.raw_storage.models.models import IncomingRaw, RawResponse
 from src.raw_storage.models.response_models import ResponseModel
 from src.raw_storage.repository.repository import RawRepository
 
@@ -12,11 +12,14 @@ class RawStorageService:
         self.raw_repository = RawRepository()
         self.response = ResponseModel()
 
-    # async def arrival_raw(self, raw: IncomingRaw):
-    #     return await self.raw_repository.add_raw(raw)
+    async def arrival_raw(self, raw: IncomingRaw):
+        self.response.data = await self.raw_repository.add_raw(raw)
+        self.response.message = f"Item {raw.title} in amount {raw.incoming_amount} added successful"
+        return self.response
 
     async def get_storage(self, name):
-        self.response.data = await self.raw_repository.get_storage(name)
+        item_from_base = await self.raw_repository.get_storage(name)
+        self.response.data = RawResponse(**item_from_base[name].model_dump())
 
         if name and not self.response.data:
             self.response.status = False
