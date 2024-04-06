a4e271ddd31664ce58cbe6fb1553b03f1c5eff4b
amoglock <amoglock@gmail.com>
2024-04-07 00:54:43 +0400

    add_raw

--- src/raw_storage/repository/repository.py
+++ src/raw_storage/repository/repository.py
@@ -3,18 +3,20 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-from src.database import SessionLocal
-from src.raw_storage.models.models import IncomingRaw, IncomingRawDB
+from src.database import engine, Raw
+from src.raw_storage.models.models import IncomingRaw
+from sqlmodel import Session
 
 
 class RawRepository:
 
     @staticmethod
-    async def add_raw(db: Session, raw: IncomingRaw,) -> IncomingRaw:
-        db.add(raw)
-        db.commit()
+    async def add_raw(incoming_raw: Raw) -> Raw:
+        with Session(engine) as session:
+            session.add(incoming_raw)
+            session.commit()
 
-        return raw
+        return incoming_raw
 
     async def get_storage(self, name):
         """
