32bd549c94ee4c90833416b30e055fa2d7fa2270
amoglock <amoglock@gmail.com>
2024-04-09 20:44:18 +0400

    collect all models to models.py

--- src/raw_storage/models/models.py
+++ src/raw_storage/models/models.py
@@ -1,12 +1,24 @@
-from datetime import date, datetime
+from datetime import date
 
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
 
-class RawResponse(BaseModel):
-    title: str = Field(description="Name of raw", examples=["Dragon Fruit"])
+class Raw(SQLModel, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    title: str
     group: str
-    supplier: str = Field(description="Name of supplier", examples=["Microsoft"])
+    supplier: str
+    is_frozen: bool
+    delivery_date: date
+    production_date: date
+    expiration_date: date
+
+
+class RawResponse(SQLModel):
+    title: str = Field(description="Name of raw")
+    group: str
+    supplier: str = Field(description="Name of supplier")
 
 
 class IncomingRaw(RawResponse):
@@ -15,3 +27,9 @@ class IncomingRaw(RawResponse):
     delivery_date: date = Field(default_factory=date.today)
     production_date: date | None = None
     expiration_date: date | None = None
+
+
+class ResponseModel(BaseModel):
+    status: bool = Field(default=True)
+    message: str | None = "success"
+    data: IncomingRaw | dict = {}
