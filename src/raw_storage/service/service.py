d493a31a238be3b16ba20bf028f826701525fac6
amoglock <amoglock@gmail.com>
2024-05-02 19:17:36 +0400

    work in progress....

--- src/raw_storage/service/service.py
+++ src/raw_storage/service/service.py
@@ -1,14 +1,20 @@
-from src.raw_storage.models.models import IncomingRaw, ResponseModel, Raw
+from src.raw_storage.models.models import IncomingRaw, ResponseModel, Vegetables, Meat, Gastronomy
 from src.raw_storage.repository.repository import RawRepository
 
 
 class RawStorageService:
+    """
+    Class for service
+    """
     def __init__(self):
         self.raw_repository = RawRepository()
-        self.response = ResponseModel
+        self.response = ResponseModel()
+        # TODO: It's boolshit)))). self.groups needs to create not from hardcoded dict.
+        self.groups = {"Vegetables": Vegetables, "Meat": Meat, "Gastronomy": Gastronomy}
 
     async def arrival_raw(self, raw: IncomingRaw):
-        incoming_raw = Raw.model_validate(raw)
+        # TODO: Add try-except for KeyError
+        incoming_raw = self.groups[raw.group.title()].model_validate(raw)
         await self.raw_repository.add_raw(incoming_raw)
 
         self.response.data = raw
@@ -16,8 +22,8 @@ class RawStorageService:
 
         return self.response
 
-    async def get_storage(self, name):
-        item_from_base = await self.raw_repository.get_storage(name)
+    async def get_storage(self, name: str, group: str):
+        item_from_base = await self.raw_repository.get_storage(name, self.groups[group.title()])
 
         if name and not item_from_base:
             self.response.status = False
@@ -25,7 +31,8 @@ class RawStorageService:
             return self.response
 
         if name:
-            self.response.data = item_from_base
+            # self.response.data = IncomingRaw.model_validate(item_from_base)
+            print(item_from_base)
             return self.response
 
         self.response.message = "No item for search"
