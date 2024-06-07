b906e40cc70bf56610d03b4835e5883f7cffb135
amoglock <amoglock@gmail.com>
2024-06-07 17:32:33 +0400

    add order and product routers

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+
+from src.orders_maker.routers.order_router import order_router
+from src.product_maker.routers.product_router import product_router
 from src.raw_storage.routers.raw_router import raw_router
 from src.database import create_db_and_tables
 
@@ -7,3 +10,5 @@ app = FastAPI()
 create_db_and_tables()
 
 app.include_router(raw_router)
+app.include_router(order_router)
+app.include_router(product_router)
