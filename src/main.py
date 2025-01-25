68e468dd53de50543a8e7cfa9413562f65b367ac
AmOglock <amoglock@gmail.com>
2025-01-26 02:27:16 +0300

    Add main router to FastAPI application

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from frontend.routers.main_router import main_router
 from src.orders_maker.routers.order_router import order_router
 from src.product_maker.routers.product_router import product_router
 from src.raw_storage.routers.raw_router import raw_router
@@ -9,6 +10,7 @@ app = FastAPI()
 
 create_db_and_tables()
 
+app.include_router(main_router)
 app.include_router(raw_router)
 app.include_router(order_router)
 app.include_router(product_router)
