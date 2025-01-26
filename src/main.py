a6b204c685c183af4fcbb1e92260f9d5e1491a37
AmOglock <amoglock@gmail.com>
2025-01-26 20:38:27 +0300

    Add static file serving for frontend assets

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from frontend.routers.main_router import main_router
 from src.orders_maker.routers.order_router import order_router
@@ -7,6 +8,7 @@ from src.raw_storage.routers.raw_router import raw_router
 from src.database import create_db_and_tables
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
 create_db_and_tables()
 
