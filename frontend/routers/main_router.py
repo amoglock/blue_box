2795e1240ad6ade018d2b897001142958d58c156
AmOglock <amoglock@gmail.com>
2025-02-01 11:30:30 +0300

    Add error handling to main router routes

--- frontend/routers/main_router.py
+++ frontend/routers/main_router.py
@@ -1,18 +1,26 @@
-from fastapi import APIRouter, Request
+from pathlib import Path
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from src.templates import templates
+
 
 main_router = APIRouter(
     prefix='',
     tags=['frontend'],
 )
 
-templates = Jinja2Templates(directory="frontend/templates")
-
 @main_router.get("/", response_class=HTMLResponse)
 async def login_page(request: Request):
-    return templates.TemplateResponse("login_page.html", {"request": request})
+    try:
+        return templates.TemplateResponse("login_page.html", {"request": request})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @main_router.get("/index", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    try:
+        return templates.TemplateResponse("index.html", {"request": request})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
